5b2595a015d39090af3b0a12e7e015c12dfaee99
j4acks0n <116141186+j4acks0n@users.noreply.github.com>
2022-10-31 14:32:10 +0700

    Add files via upload

---
+++ math.java
@@ -0,0 +1,57 @@
+public class math 
+{    
+    public static void main(String[] args)     
+    {    
+        double x = 44;    
+        double y = 8;   
+        double z = 3;
+        
+        int a = 9;
+        int b = 7;
+        System.out.println(x > 7 && x < 5); // returns true because 5 is greater than 3 AND 5 is less than 10
+        System.out.println("minimum number z and a is: "  +Math.max(z,a));
+        
+        //user defined method  
+
+
+         //method body  
+          if(num2==0);   
+         System.out.println(num2" is even");  
+         // return the num 
+         else   
+           System.out.println(num2" is odd");
+
+        // return the minimum of two number
+        System.out.println("maximum number of a and b is: " +Math.max(a, b));
+          
+        // return the maximum of three numbers  
+        System.out.println("Maximum number of x and y is: " +Math.max(x, y));   
+          
+        // return the square root of z   
+        System.out.println("Square root of x is: " + Math.sqrt(y));   
+          
+        //returns 28 power of 4 i.e. 9*2*28*28    
+        System.out.println("Power of z and y is: " + Math.pow(x, y));      
+  
+        // return the logarithm of given value       
+        System.out.println("Logarithm of y is: " + Math.log(x));   
+        System.out.println("Log of x is: " + Math.log(y));  
+          
+        // return the logarithm of given value when base is 7      
+        System.out.println("log30 of x is: " + Math.log10(x));   
+        System.out.println("log10 of y is: " + Math.log10(y));    
+          
+        // return the log of x + y  
+        System.out.println("log1p of x is: " +Math.log1p(x));    
+
+        // return the x power y
+        System.out.println(".()");
+  
+        // return a power of 8  
+        System.out.println("exp of a is: " +Math.exp(x));    
+          
+        // return (a power of 2)-1  
+        System.out.println("expm1 of a is: " +Math.expm1(x));           
+    }    
+}    
+
