c8d0c01b1559cbb75c129ae083f4fe1fef7793cd
j4acks0n <116141186+j4acks0n@users.noreply.github.com>
2022-11-02 03:19:44 +0700

    Update math.java

--- math.java
+++ math.java
@@ -1,6 +1,6 @@
 public class math {
     public static void main(String args[]) {
-
+           // code here can't use z
            int x = 100;
          Number y = 200; 
          int  z =abc(-7.25);
@@ -10,6 +10,9 @@ public class math {
           System.out.println(y);
           float f1 = 35e3f;
           double d1 = 12E4d;
+           // char
+          var d =  1.22;
+          System.out.println(d);
           System.out.println(f1);
           System.out.println(d1);
     }
