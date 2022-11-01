825f28c7d99bbe6dd6efdb6bbcdf4df6bda0a946
j4acks0n <116141186+j4acks0n@users.noreply.github.com>
2022-11-02 03:10:16 +0700

    Add files via upload

---
+++ math.java
@@ -0,0 +1,20 @@
+public class math {
+    public static void main(String args[]) {
+
+           int x = 100;
+         Number y = 200; 
+         int  z =abc(-7.25);
+         System.out.println(z);
+         // Code here can use x
+         System.out.println(x);
+          System.out.println(y);
+          float f1 = 35e3f;
+          double d1 = 12E4d;
+          System.out.println(f1);
+          System.out.println(d1);
+    }
+
+    private static int abc(double d) {
+        return 0;
+    }
+}
