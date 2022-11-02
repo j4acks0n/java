ddbd903be87aa014ec9c8cbf4dd78262bf21954a
j4acks0n <116141186+j4acks0n@users.noreply.github.com>
2022-11-02 21:16:27 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,20 @@
+import java.lang.Math;
+public class main{
+    
+    public static void main(String[] args){
+    
+
+     int a = 3;
+     int b = 7;
+     int c = 4;
+      //float d = 2.0;
+      System.out.println(a);
+      System.out.println(a/b);
+
+
+
+    
+    }
+    
+
+}
