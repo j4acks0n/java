19f604fff004ae1b1b92d53795618eeaf995b610
j4acks0n <116141186+j4acks0n@users.noreply.github.com>
2022-11-08 01:41:40 +0700

    Add files via upload

---
+++ Untitled-122.py
@@ -0,0 +1,14 @@
+b = 7
+c = 4
+d = 6
+a = 8
+int = f = 66
+numbers1 = [1, 2, 3]
+numbers2 = [4, 5, 6]
+numbers3 = [7,8,9]
+merged = numbers1 + numbers2 + numbers3
+print(merged)
+print(b/c)
+print(d//b)
+print(d)
+print(float(86+3))
---
+++ main1.java
@@ -0,0 +1,23 @@
+import java.lang.Math;
+public class main{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+    
+      float e = 89;
+     int a = 6;
+     int b = 7;
+     int c = 4;
+     double f = 9;
+      //float d = 2.0;
+      System.out.println(a);
+      System.out.println(a/b);
+      System.out.println(e);
+      System.out.println(f);
+
+
+    
+    }
+  }  
